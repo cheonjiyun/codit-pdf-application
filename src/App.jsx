97a80b38c2f9ef0a888ed8e5c402ab6b4f7d1143
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-06-22 14:52:44 +0900

    feat: pdf만 업로드할 수 있도록 제한

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
 
 function App() {
+    const filtTypeCheck = (event) => {
+        const fileType = event.target.value.split(".");
+        if (fileType[fileType.length - 1] === "pdf") {
+            return true;
+        }
+        alert("pdf만 업로드 해주세요.");
+    };
+
     return (
         <main>
-            <input type="file" id="pdf-upload" />
+            <input type="file" id="pdf-upload" accept=".pdf" onChange={filtTypeCheck} />
             <label htmlFor="pdf-upload">PDF업로드</label>
         </main>
     );
